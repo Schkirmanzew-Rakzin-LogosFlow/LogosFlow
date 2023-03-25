a3a2cb297b6acf7b64803d04d1370575a25e5b30
Dimitri Schkirmanzew <shkirmantsev@gmail.com>
2023-03-25 14:49:43 +0100

    Implement PKCE Auth-flow Single SignOn
    refs #7

---
+++ spa-demo-endpoint/src/main/resources/static/scripts/oidc/oidc-v1-m-Oauth2LoginLogoutManager.js
@@ -0,0 +1,75 @@
+/**
+  @author: Dmytro Shkirmantsev <shkirmantsev@gmail.com>
+*/
+import AuthorizationUriBuilder from "./oidc-v1-m-AuthorizationUriBuilder.js";
+import AccesTokenManager from "./oidc-v1-m-AccesTokenManager.js";
+import OidcTokensStorage from "./oidc-v1-m-OidcTokensStorage.js";
+import { OidcClientConfiguration } from "./oidc-v1-pkce-lib.js";
+import PageStateStorage from "./oidc-v1-m-PageStateStorage.js";
+import PkceCodeChallengeVerifierGenerator from "./oidc-v1-m-PkceCodeChallengeVerifierGenerator.js";
+import PageStateGenerator from "./oidc-v1-m-PageStateGenerator.js";
+import PkceCodeChallengeVerifierStorage from "./oidc-v1-m-PkceCodeChallengeVerifierStorage.js";
+import RedirectUserToAuthorization from "./oidc-v1-m-RedirectUserToAuthorization.js";
+
+
+export class Oauth2LoginLogoutManager {
+
+  static LOGIN_TIMEOUT_MS = 100;
+
+  static async login() {
+    try {
+      if (await AccesTokenManager.isAuthenticationSuccessful()) {
+        return;
+      }
+
+      await Oauth2LoginLogoutManager.forceLogin();
+    } catch (error) {
+      console.error(error);
+      Oauth2LoginLogoutManager.logout();
+    }
+  }
+
+  static async forceLogin() {
+    OidcTokensStorage.clear();
+
+    let state = PageStateGenerator.generateState();
+    state = PageStateStorage.storeOauth2State(state);
+
+    let codeVerifier = PkceCodeChallengeVerifierGenerator.generateCodeVerifier();
+    codeVerifier = PkceCodeChallengeVerifierStorage.storePkceCodeVerifier(codeVerifier);
+
+    let codeChallenge = await PkceCodeChallengeVerifierGenerator.generateCodeChallengeValue(codeVerifier);
+    codeChallenge = PkceCodeChallengeVerifierStorage.storeOauth2PkceCodeChallenge(codeChallenge);
+
+    let authorizationUrlWithPromptUi = AuthorizationUriBuilder.buildUriFor(
+      await OidcClientConfiguration.get(),
+      state,
+      codeChallenge
+    );
+
+    RedirectUserToAuthorization.redirectToUrl(authorizationUrlWithPromptUi);
+
+    await Oauth2LoginLogoutManager.waitUntilLoginIsSuccessful();
+  }
+
+  static async waitUntilLoginIsSuccessful() {
+    let isAuthenticationSuccessful = await AccesTokenManager.isAuthenticationSuccessful();
+    if (isAuthenticationSuccessful) {
+      return;
+    }
+    await new Promise(resolve => setTimeout(resolve, Oauth2LoginLogoutManager.LOGIN_TIMEOUT_MS));
+    await Oauth2LoginLogoutManager.waitUntilLoginIsSuccessful();
+  }
+
+  static logout() {
+    Oauth2LoginLogoutManager.clearOauth2StoredData();
+  }
+
+  static clearOauth2StoredData() {
+    PageStateStorage.clear();
+    PkceCodeChallengeVerifierStorage.clearPair();
+    OidcTokensStorage.clear();
+  }
+};
+
+export default Oauth2LoginLogoutManager;
---
+++ spa-demo-endpoint/src/main/resources/static/scripts/oidc/oidc-v1-m-onOauth2AuthorizeHandler.js
@@ -0,0 +1,43 @@
+/**
+  @author: Dmytro Shkirmantsev <shkirmantsev@gmail.com>
+*/
+import PageStateStorage from "./oidc-v1-m-PageStateStorage.js";
+import OidcTokensStorage from "./oidc-v1-m-OidcTokensStorage.js";
+import Oauth2LoginLogoutManager from "./oidc-v1-m-Oauth2LoginLogoutManager.js";
+import OidcTokenExchanger from "./oidc-v1-m-OidcTokenExchanger.js";
+
+
+class OnOAuth2AuthorizedHandler {
+
+  static async handle(state, authCode, dialogWindow) {
+
+    const originalStateValue = PageStateStorage.retrieveOauth2State();
+
+    if (!originalStateValue) {
+      throw new Error("Failed to retrieve OAuth2State during browser authorization flow");
+    }
+
+    try {
+      if (state === originalStateValue) {
+        const oidcTokens = await OidcTokenExchanger.exchangeCodeOnOidcTokens(authCode);
+        OidcTokensStorage.storeOidcTokens(oidcTokens);
+        return;
+      }
+    } catch (error) {
+      console.log("Error exchanging code for OIDC Tokens", error);
+      Oauth2LoginLogoutManager.logout();
+    } finally {
+      closeAuthDialogWindow(dialogWindow);
+    }
+
+    Oauth2LoginLogoutManager.logout();
+
+    function closeAuthDialogWindow(dialogWindow) {
+      try {
+        dialogWindow.close();
+      } catch (error) { console.error(error); }
+    }
+  };
+}
+
+export default OnOAuth2AuthorizedHandler;
