2090f552c5142a63e2bdf48068a27dc980606ef2
Dimitri Schkirmanzew <shkirmantsev@gmail.com>
2024-06-13 22:45:49 +0200

    Define ContentSource
    refs #14

---
+++ logosflow-content-repository/src/main/java/io/logosflow/modules/app/contentrepository/infrastructure/content_sources/ContentSource.java
@@ -0,0 +1,11 @@
+package io.logosflow.modules.app.contentrepository.infrastructure.content_sources;
+
+/**
+ * This interface is only the marker of the specific content source
+ *
+ * @author Shkirmantsev
+ */
+public interface ContentSource {
+
+    String getContentSourceType();
+}
