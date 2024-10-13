4d8f4de07377e8578b81629b09c1c67a66829fec
Dimitri Schkirmanzew <shkirmantsev@gmail.com>
2024-10-13 20:16:54 +0200

    Update dependencies
    refs #14

--- logosflow-gateway/src/main/java/io/logosflow/modules/LogosFlowGatewayApplication.java
+++ logosflow-gateway/src/main/java/io/logosflow/modules/LogosFlowGatewayApplication.java
@@ -2,17 +2,15 @@ package io.logosflow.modules;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Shkirmantsev
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
-public class LogosFlowGatewayApplication
-{
-    public static void main( String[] args )
-    {
+public class LogosFlowGatewayApplication {
+    public static void main(String[] args) {
         SpringApplication.run(LogosFlowGatewayApplication.class, args);
     }
 }
