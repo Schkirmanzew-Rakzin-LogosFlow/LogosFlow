4d5c30a855ac23f12964205420ae5b97e71582c1
Dimitri Schkirmanzew <shkirmantsev@gmail.com>
2025-12-28 21:18:49 +0100

    Prepare ontentMetaInfoConstants

---
+++ logosflow-content-repository/src/main/java/io/logosflow/modules/app/contentrepository/models/content/ContentMetaInfoConstants.java
@@ -0,0 +1,16 @@
+package io.logosflow.modules.app.contentrepository.models.content;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.http.MediaType;
+
+/**
+ * @author Shkirmantsev
+ */
+@UtilityClass
+public class ContentMetaInfoConstants {
+
+    public static final MediaType USED_DEFAULT_MEDIA_TYPE = MediaType.APPLICATION_OCTET_STREAM;
+    public static final String USED_DEFAULT_MEDIA_TYPE_VALUE = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+
+
+}
